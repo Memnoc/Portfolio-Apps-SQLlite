1ad0ae1d11e02879f1109643ef5c1858529c94dc
Memnoc <smartdroidesign@gmail.com>
2019-03-13 11:57:15 +0000

    JAVA - created database JAVA - created database records JAVA - retrieved data with a Cursor

--- app/src/main/java/com/smartdroidesign/sqllitemanager/MainActivity.java
+++ app/src/main/java/com/smartdroidesign/sqllitemanager/MainActivity.java
@@ -7,11 +7,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +25,28 @@ public class MainActivity extends AppCompatActivity {
 
         // Populating the database
         SQLiteDatabase sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqlite-test-1.db", MODE_PRIVATE, null);
-        sqLiteDatabase.execSQL("CREATE TABLE contacts(name TEXT, phone INTEGER, email TEXT)");
-        sqLiteDatabase.execSQL("INSERT INTO contacts VALUES('Matteo', 66236492, 'matteo@mamail.com');");
-        sqLiteDatabase.execSQL("INSERT INTO contacts VALUES('John', 749823075, 'john@mamail.com');");
+        String sql = "CREATE TABLE IF NOT EXISTS contacts(name TEXT, phone INTEGER, email TEXT)";
+        Log.d(TAG, "onCreate: sql is " + sql);
+        sqLiteDatabase.execSQL(sql);
+        sql = "INSERT INTO contacts VALUES('Matteo', 66236492, 'matteo@mamail.com')";
+        sqLiteDatabase.execSQL(sql);
+        Log.d(TAG, "onCreate: sql is " + sql);
+        sql = "INSERT INTO contacts VALUES('John', 749823075, 'john@mamail.com');";
+        sqLiteDatabase.execSQL(sql);
+        Log.d(TAG, "onCreate: sql is " + sql);
 
         // Retrieving the data
         Cursor query = sqLiteDatabase.rawQuery("SELECT * FROM contacts;", null);
-        if(query.moveToFirst()) {
-            String name = query.getString(0);
-            int phone = query.getInt(1);
-            String email = query.getString(2);
+        if (query.moveToFirst()) {
+            do {
+                String name = query.getString(0);
+                int phone = query.getInt(1);
+                String email = query.getString(2);
+                Toast.makeText(this, "Name = " + name + " Phone =" + phone + " Email =" + email, Toast.LENGTH_LONG).show();
+            } while (query.moveToNext());
         }
+        query.close(); // cursor closed
+        sqLiteDatabase.close(); // -> database closed
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -44,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,4 +81,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
